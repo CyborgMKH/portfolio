ae4ea4fffefdd7816cd472d6bb33a7642cd3e1b7
nirajacharyaa <mail.nirajacharya@gmail.com>
2023-07-14 15:17:43 +0545

    feat: :sparkles: changed the router to hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import { AcademicCapIcon, ExternalLinkIcon, MenuIcon } from '@heroicons/react/outline'
-import React, { useEffect, useRef, useState } from 'react'
-import { BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom'
-import { CSSTransition } from 'react-transition-group'
-import GPG from './views/GPG'
-import Home from './views/Home'
-import NotFound from './views/NotFound'
-import Projects from './views/Projects'
-import Social from './views/Social'
+import {
+  AcademicCapIcon,
+  ExternalLinkIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes,
+  HashRouter,
+  createHashRouter,
+} from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+import GPG from "./views/GPG";
+import Home from "./views/Home";
+import NotFound from "./views/NotFound";
+import Projects from "./views/Projects";
+import Social from "./views/Social";
 
 const App = () => {
-  const currentYear: number = new Date().getFullYear()
-  const [showMenu, setShowMenu] = useState(false)
+  const currentYear: number = new Date().getFullYear();
+  const [showMenu, setShowMenu] = useState(false);
 
   // TO-DO: this should be refactored
-  const menuRef = useRef<any>(null)
-  const buttonRef = useRef<any>(null)
+  const menuRef = useRef<any>(null);
+  const buttonRef = useRef<any>(null);
 
   const closeMenu = () => {
-    setShowMenu(false)
-  }
+    setShowMenu(false);
+  };
   const handleToggleMenu = (e: MouseEvent) => {
     if (buttonRef.current && buttonRef.current.contains(e.target)) {
       return;
@@ -29,17 +40,17 @@ const App = () => {
   };
 
   useEffect(() => {
-    document.addEventListener('click', handleToggleMenu)
+    document.addEventListener("click", handleToggleMenu);
     return () => {
-      document.removeEventListener('click', handleToggleMenu)
-    }
+      document.removeEventListener("click", handleToggleMenu);
+    };
   }, []);
 
   return (
-    <Router>
+    <HashRouter basename="/">
       <div className="min-h-screen flex flex-col dark:bg-gray-900 dark:text-white">
         <div className="container mx-auto px-6 flex-grow max-w-5xl">
-          <nav className="mt-4 flex flex-row justify-between">
+          <nav className="mt-4 flex flex-row items-center justify-between">
             <Link
               className="flex lg:p-2 py-2 rounded items-center lg:hover:bg-gray-100 lg:dark:hover:bg-gray-800"
               to="/"
@@ -81,12 +92,18 @@ const App = () => {
               className="md:hidden p-2"
               ref={buttonRef}
               onClick={() => {
-                setShowMenu(true)
+                setShowMenu(true);
               }}
             >
               <MenuIcon className="w-5 h-5" />
             </button>
-            <CSSTransition in={showMenu} timeout={300} classNames="menu" unmountOnExit nodeRef={menuRef}>
+            <CSSTransition
+              in={showMenu}
+              timeout={300}
+              classNames="menu"
+              unmountOnExit
+              nodeRef={menuRef}
+            >
               <div className="absolute top-0 right-0" ref={menuRef}>
                 <div className="flex flex-col space-y-4 m-3 p-4 rounded bg-white dark:bg-gray-800 shadow-xl">
                   <Link to="/projects" onClick={closeMenu}>
@@ -98,7 +115,11 @@ const App = () => {
                   <Link to="/gpg" onClick={closeMenu}>
                     Contact
                   </Link>
-                  <a className="flex items-center" href="mailto:suman.mukhiya@hotmail.com" onClick={closeMenu}>
+                  <a
+                    className="flex items-center"
+                    href="mailto:suman.mukhiya@hotmail.com"
+                    onClick={closeMenu}
+                  >
                     <span className="mr-2">Get in touch</span>
                     <ExternalLinkIcon className="w-5 h-5" />
                   </a>
@@ -108,33 +129,36 @@ const App = () => {
           </nav>
 
           <div className="my-16">
-          <Routes>
-              <Route path="/projects" element={<Projects />} /> {/* Wrap Projects component with Route */}
-              <Route path="/social" element={<Social />} /> {/* Wrap Social component with Route */}
-              <Route path="/gpg" element={<GPG />} /> {/* Wrap GPG component with Route */}
-              <Route path="/" element={<Home />} /> {/* Use "element" prop for the root path */}
-              <Route path="*" element={<NotFound />} /> {/* Use "element" prop for the wildcard path */}
+            <Routes>
+              <Route path="/" Component={Home} />
+              <Route path="/projects" Component={Projects} />
+              <Route path="/social" Component={Social} />
+              <Route path="/gpg" Component={GPG} />
+              <Route path="*" Component={NotFound} />
             </Routes>
           </div>
         </div>
 
-        <footer className="w-full text-center bg-gray-800 text-gray-400 p-4">
+        <footer className="w-full text-center bg-white dark:bg-gray-800 dark:text-gray-400 text-black p-4">
           <div className="container mx-auto">
             <div>
-              Powered by{' '}
+              Powered by{" "}
               <a className="hover:text-white" href="https://reactjs.org/">
                 React
               </a>
-              ,{' '}
+              ,{" "}
               <a className="hover:text-white" href="https://tailwindcss.com/">
                 Tailwind CSS
               </a>
-              ,{' '}
+              ,{" "}
               <a className="hover:text-white" href="https://vitejs.dev/">
                 Vite
-              </a>{' '}
-              and{' '}
-              <a className="hover:text-white" href="https://www.typescriptlang.org/">
+              </a>{" "}
+              and{" "}
+              <a
+                className="hover:text-white"
+                href="https://www.typescriptlang.org/"
+              >
                 TypeScript.
               </a>
             </div>
@@ -142,7 +166,7 @@ const App = () => {
           </div>
         </footer>
       </div>
-    </Router>
+    </HashRouter>
   );
 };
 
