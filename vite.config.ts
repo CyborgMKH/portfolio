649fcf5ed4071f582ccc6fa8c7b79cdf44380b44
cyborgmkh <sumanmukhiya@ismt.edu.np>
2023-07-14 12:57:15 +0545

    uffffffffffffffffff

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-// Custom type definition for the postcss plugin
-interface PostcssPlugin {
-  postcssPlugin: string;
-  plugins: any[];
-}
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   css: {
     postcss: {
-      plugins: [tailwindcss] as unknown as PostcssPlugin[],
+      plugins: [tailwindcss, autoprefixer],
     },
   },
 });
